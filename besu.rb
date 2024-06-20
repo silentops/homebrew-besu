fcbe903614fed2622b9aa48a1e8a232a09572431
cdivitotawela <cdivitotawela@gmail.com>
2024-06-20 05:46:50 +0000

    Update version to 24.5.4
    Signed-off-by: cdivitotawela <cdivitotawela@gmail.com>

--- besu.rb
+++ besu.rb
@@ -1,9 +1,9 @@
 class Besu < Formula
   desc "hyperledger besu ethereum client"
   homepage "https://github.com/hyperledger/besu"
-  url "https://github.com/hyperledger/besu/releases/download/24.6.0/besu-24.6.0.zip"
+  url "https://github.com/hyperledger/besu/releases/download/24.5.4/besu-24.5.4.zip"
   # update with: ./updateBesu.sh <new-version>
-  sha256 "8b2d3a674cd7ead68b9ca68fea21e46d5ec9b278bbadc73f8c13c6a1e1bc0e4d"
+  sha256 "9d8774a69f90986477aabd2d7713ed22df1e2f29ed6b62a1889ac21675026f83"
 
   depends_on "openjdk" => "21+"
 
